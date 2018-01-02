dcc0f05a1a3dafe297db10a8a9dc5f1bf3074656
janus_wel <janus.wel.3@gmail.com>
2018-01-02 18:51:33 +0900

    :sparkles: fix interfaces

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 // @flow
 
-import ajv from 'ajv'
+import minimist from 'minimist'
+import fs from 'fs'
+
+const generateDefinitions = async definitions => {
+  console.log(Object.keys(definitions))
+}
+const generateLinks = async links => {
+  console.log(links)
+}
+
+const main = async () => {
+  const argv = minimist(process.argv.slice(2))
+  const raw = fs.readFileSync(argv._[0]).toString()
+
+  const schema = JSON.parse(raw)
+
+  await generateDefinitions(schema.definitions)
+  await generateLinks(schema.links)
+}
+
+main()
